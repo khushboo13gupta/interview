5edd821b9a28a8deb73cb7bd8daabcc6c7859d7a
khushboo13gupta <khushboo13gupta@gmail.com>
2026-06-25 21:31:27 +0530

    initial commit

---
+++ example/src/test/java/com/solution/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solution.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
